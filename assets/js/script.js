97b930af896db451c4ea50cadb98107b71944ff2
Brianna Hebeler <briannahebeler@gmail.com>
2020-11-17 17:54:49 -0500

    got icon to show up

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,14 @@ $(document).ready(function () {
 
     $("#search-button").on("click", function () {
         var searchValue = $("#search-value").val();
+        // var lat =;
+        // var lon =;
 
         $("search-value").val("");
 
         searchWeather(searchValue);
-        searchUV();
-        // search5Day();
+        searchUV(searchValue);
+        // search5Day(searchValue);
     })
 
 
@@ -25,25 +27,38 @@ $(document).ready(function () {
             $("#today").empty();
 
             //creating a card for appending weather data
-            var title = $("<h3>").addClass("card-title").text(data.name + " (" + moment().format('L') + ")" + " " + data.weather[0].icon);
+            var title = $("<h3>").addClass("card-title").text(data.name + " (" + moment().format('L') + ")" + " ");
+            var iconURL = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+            var icon = $("<img>").attr("src", iconURL);
             var card = $("<div>").addClass("card");
             var temp = $("<p>").addClass("card-text").text("Temp: " + data.main.temp);
             var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed);
             var humid = $("<p>").addClass("card-text").text(`Humidity: ${data.main.humidity}`);
-            var cardBody = $("<div>").addClass("card-body searchWeather");
+            var cardBody = $("<div>").addClass("card-body").attr("id", "searchWeather");
 
-            cardBody.append(title, temp, humid, wind);
+            cardBody.append(title, icon, temp, humid, wind);
             card.append(cardBody);
             $("#today").append(card);
 
         });
     }
 
-    function searchUV() {
-        cardBody = $(".searchWeather");
-        var uv = $("<p>").addClass("card-text").text(`UV Index: `);
-        cardBody.append(uv);
+    function searchUV(lat, long) {
+        $.ajax({
+            type: "GET",
+            url: `http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=c691cfbf611a02788a2576d8d581c1c7&units=imperial`,
+            dataType: "json",
+        }).then(function(data) {
+            console.log(data);
 
+            //creating a new p tag for the data appended to the cardbody class of the previous
+            console.log("i see in here")
+            var cardBody = $("#searchWeather");
+            var uv = $("<p>").addClass("card-text").text(`UV Index: `);
+
+            cardBody.append(uv);
+
+        });
     }
 
     // function search5Day() {
